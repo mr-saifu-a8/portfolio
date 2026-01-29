e0ff904ee1673efde7f0769248713567f8e6722e
mrsaifu08 <saifstack25@gmail.com>
2026-01-29 15:50:34 +0530

    Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="flex items-center flex-col justify-center">
+        <p className="text-white text-4xl font-extralight">
+          Hey, I'm Saif! welcom to my
+        </p>
+        <div>
+          <h1 className='text-white font-["fnt"] text-[8.5vw]'>
+            portfolio
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
