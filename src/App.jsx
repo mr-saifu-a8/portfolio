9028db6b584963a0973ff19249b0505dee7777b1
mrsaifu08 <saifstack25@gmail.com>
2026-01-29 15:48:25 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import MenuBar from './components/MenuBar'
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <>
+      <div
+        className="
+        w-full h-screen
+        bg-[url('public/macos-tahoe-26-5120x2880-22674.jpg')]
+        bg-cover bg-center bg-no-repeat
+        relative
+      "
+      >
+        <Navbar />
+        <div className='flex items-center justify-center'>
+          <MenuBar />
+        </div>
+        <Hero/>
+      </div>
+    </>
+  );
+}
+
+export default App
